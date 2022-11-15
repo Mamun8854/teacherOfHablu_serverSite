d9bba309a9748608ed6fd50237dca0557428a3ef
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-16 02:08:22 +0600

    data read from mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nuhoilk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("teacherOfHablu")
+      .collection("services");
+
+    app.get("/services", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.limit(size).sort({ date: -1 }).toArray();
+      const count = await serviceCollection.estimatedDocumentCount();
+      res.send({ count, service });
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.log(error));
 app.get("/", (req, res) => {
   res.send("Assignment 11 running");
 });
